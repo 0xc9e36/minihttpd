7504ec9d6511afd8aeb9e6e7a4b3a4338941d556
tw1996 <2698143402@qq.com>
2017-05-22 20:18:29 +0800

    list

--- src/list.h
+++ src/list.h
@@ -1,7 +1,7 @@
 #ifndef _LIST_HEAD_H_
 #define _LIST_HEAD_H_
 
-/* 参考linux内核实现, 侵入式双链表, 十分巧妙, 更多参考 : http://www.cnblogs.com/skywang12345/p/3562146.html */
+/* 参考linux内核实现, 侵入式双链表, 十分巧妙, 更多参考大佬文章 : http://www.cnblogs.com/skywang12345/p/3562146.html */
 
 typedef struct list_head{
 	struct list_head *next, *prev;
@@ -40,7 +40,7 @@ static inline void list_add(struct list_head *new, struct list_head *head){
 
 /* 尾插法 */
 static inline void list_add_tail(struct list_head *new, struct list_head *head){
-	_list_add(head->prev, head);
+	_list_add(new, head->prev, head);
 }
 
 static inline void _list_del(struct list_head *prev, struct list_head *next){
@@ -58,7 +58,7 @@ static inline void _list_del_entry(struct list_head *entry){
 }
 
 /* 删除节点, 并让节点的前驱和后继都指向自己 */
-static inline void list_del_init(list_head *eentry){
+static inline void list_del_init(list_head *entry){
 	_list_del_entry(entry);
 	INIT_LIST_HEAD(entry);
 }
@@ -69,13 +69,13 @@ static inline list_empty(const struct list_head *head){
 }
 
 /* 获取MEMBER在结构体TYPE中的偏移 */
-#define offsetof(TYPE, MEMBER) (size_t) &((TYPE *)0->MEMBER)
+#define offsetof(type, member) ((size_t) &((type *)0)->member)
 
 /* 根据域成员变量(member)的指针(ptr)获取整个结构体(type)变量指针 */
 /* 定义了一个不可变的_mpt指针, 指向的内容是type结构体中的member成员地址. 然后用_mptr地址减去_mptr指针的偏移就得到了type的地址 */
 #define container_of(ptr, type, member) ({	\
 		const typeof( ((type *)0)->member) * _mptr = (ptr);	\
-		(type *)( (char *)_mptr - offset(type, member) );})
+		(type *)( (char *)_mptr - offsetof(type, member) );})
 
 #define list_entry(ptr, type, member)	\
 		container_of(ptr, type, member)
--- tests/list_test.c
+++ tests/list_test.c
@@ -1,8 +1,61 @@
-#include "../src/list.h"
+#include <stdio.h> 
+#include <stdlib.h>
+#include <string.h>
+#include "../src/list.h" 
 
-#include<stdio.h>
+struct person { 
+    int age; 
+    char name[20];
+    struct list_head list; 
+};
 
-int main(void){
-	
+int main(void) { 
+    struct person *person_node; 
+    struct person person_head; 
+    struct list_head *pos, *next; 
+    int i;
 
+    // 初始化双链表的表头 
+    INIT_LIST_HEAD(&person_head.list); 
+
+    // 添加节点
+    for ( i = 0; i < 5; i++){
+        person_node = (struct person*)malloc(sizeof(struct person));
+        person_node->age = (i+1)*10;
+        sprintf(person_node->name, "用户%d", i+1);
+        list_add_tail(&(person_node->list), &(person_head.list));
+    }
+
+    // 遍历链表
+    printf("节点插入完毕, 开始遍历\n"); 
+    list_for_each(pos, &person_head.list) { 
+		/* 根据结构体成员获取结构体地址 */
+        person_node = list_entry(pos, struct person, list); 
+        printf("name:%-2s, age:%d\n", person_node->name, person_node->age); 
+    } 
+    
+	/* 删除age为20额节点*/
+	list_for_each_safe(pos, next, &person_head.list){
+		person_node = list_entry(pos, struct person, list);
+		if(20 == person_node->age){
+			list_del_init(pos);
+			free(person_node);
+		}
+	}
+
+    // 遍历链表
+    printf("删除完毕, 开始遍历\n"); 
+    list_for_each(pos, &person_head.list) { 
+        person_node = list_entry(pos, struct person, list); 
+        printf("name:%-2s, age:%d\n", person_node->name, person_node->age); 
+	}
+
+	/* 释放内存 */
+	list_for_each_safe(pos, next, &person_head.list){
+		person_node = list_entry(pos, struct person, list);
+		list_del_init(pos);
+		free(person_node);
+	}
+
+	return 0;
 }
